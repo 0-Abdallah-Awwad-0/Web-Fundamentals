f7d7c49c8f50d6083fbd080edcab45c76ce3b468
0-Abdallah-Awwad-0 <abdallah22awwad11@gmail.com>
2026-04-06 01:00:18 +0300

    Remove large video files to fix push error

--- JavaScript/MakingConnections/script.js
+++ JavaScript/MakingConnections/script.js
@@ -1,31 +1,30 @@
 let editLink = document.querySelector("#editProfile");
 let userName = document.querySelector("#name");
 let requestBadge = document.querySelector(".col-1 .card:first-child .badge");
-let connectionBadge = document.querySelector(".col-1 .card:nth-child(2) .badge");
+let connectionBadge = document.querySelector(
+  ".col-1 .card:nth-child(2) .badge",
+);
 //change Name
 editLink.addEventListener("click", function (e) {
-    let newName = prompt("Enter a new name:");
-    if (newName !== "" ) {
-        userName.innerText = newName;
-    }
+  let newName = prompt("Enter a new name:");
+  if (newName !== "") {
+    userName.innerText = newName;
+  }
 });
+function remove_Req(element) {
+  let listItem = element.parentElement.parentElement;
+  listItem.remove();
+
+  requestBadge.innerText = Number(requestBadge.innerText) - 1;
+}
 
 // accept request
 function accept(element) {
-    console.log("hello")
-    let listItem = element.parentElement.parentElement;
-    listItem.remove();
-
-    requestBadge.innerText = Number(requestBadge.innerText) - 1;
-    connectionBadge.innerText = Number(connectionBadge.innerText) + 1;
-    
+  remove_Req(element);
+  connectionBadge.innerText = Number(connectionBadge.innerText) + 1;
 }
 
 // remove request
 function unaccept(element) {
-    let listItem = element.parentElement.parentElement;
-    listItem.remove();
-
-    requestBadge.innerText = Number(requestBadge.innerText) - 1;
-    
+  remove_Req(element);
 }
